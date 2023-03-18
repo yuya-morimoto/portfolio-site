9f5a75387ccc123a9cf948e5fe46517d0e20f920
developeeeer <gyu2243dev@gmail.com>
2023-03-18 18:34:13 +0900

    fix: cicd

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,4 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-  build: {
-    env: {
-      PUBLIC_USERNAME: process.env.PUBLIC_USERNAME || "Unknown",
-    },
-  },
-});
+export default defineConfig({});
