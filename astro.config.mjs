c96b4fec05ccb626e2a2fc8218651d380059cc1b
developeeeer <gyu2243dev@gmail.com>
2023-03-18 18:19:50 +0900

    update: astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    env: {
+      PUBLIC_USERNAME: process.env.PUBLIC_USERNAME || "Unknown",
+    },
+  },
+});
