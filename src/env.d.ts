c9f2c52af47f5ec0821ec77ea1dab3d7bace7b55
developeeeer <gyu2243dev@gmail.com>
2023-03-18 15:19:00 +0900

    add: env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,12 @@
-/// <reference types="astro/client" />
+interface ImportMetaEnv {
+  readonly PUBLIC_USERNAME: string;
+  readonly PUBLIC_DESCRIPTION: string;
+  readonly PUBLIC_CONTACT_EMAIL_ADDRESS: string;
+  readonly PUBLIC_CONTACT_TWITTER_NAME: string;
+  readonly PUBLIC_CONTACT_GITHUB_NAME: string;
+  readonly PUBLIC_CONTACT_ZENN_NAME: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
